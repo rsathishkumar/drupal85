38db00b7b41b8ecd7078f1d35abd44d3cfba5ecb
Red Crackle <sathish2006cbe@gmail.com>
2020-01-09 19:53:32 +0530

    revert 3102402

--- core/modules/media_library/tests/src/FunctionalJavascript/EntityReferenceWidgetTest.php
+++ core/modules/media_library/tests/src/FunctionalJavascript/EntityReferenceWidgetTest.php
@@ -191,10 +191,11 @@ class EntityReferenceWidgetTest extends MediaLibraryTestBase {
     // Assert the focus is set back on the open button of the media field.
     $this->assertJsCondition('jQuery("#field_twin_media-media-library-wrapper .js-media-library-open-button").is(":focus")');
 
-    // The toggle for weight inputs visibility should not be available when
-    // the field contains a single item.
+    // Assert that we can toggle the visibility of the weight inputs.
     $wrapper = $assert_session->elementExists('css', '.field--name-field-twin-media');
-    $assert_session->buttonNotExists('Show media item weights', $wrapper);
+    $wrapper->pressButton('Show media item weights');
+    $assert_session->fieldExists('Weight', $wrapper)->click();
+    $wrapper->pressButton('Hide media item weights');
 
     // Remove the selected item.
     $button = $assert_session->buttonExists('Remove', $wrapper);
@@ -211,13 +212,6 @@ class EntityReferenceWidgetTest extends MediaLibraryTestBase {
     $this->openMediaLibraryForField('field_twin_media');
     $page->checkField('Select Dog');
     $this->pressInsertSelected('Added one media item.');
-
-    // Assert that we can toggle the visibility of the weight inputs when the
-    // field contains more than one item.
-    $wrapper = $assert_session->elementExists('css', '.field--name-field-twin-media');
-    $wrapper->pressButton('Show media item weights');
-    $assert_session->fieldExists('Weight', $wrapper)->click();
-    $wrapper->pressButton('Hide media item weights');
 
     // Assert the same has been added twice and remove the items again.
     $this->waitForElementsCount('css', '.field--name-field-twin-media [data-media-library-item-delta]', 2);
