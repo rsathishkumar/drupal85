891dc7f5749c308ef60e69acf61231da179e88ce
Red Crackle <sathish2006cbe@gmail.com>
2020-03-17 19:38:50 +0530

    issue 2851029

--- core/modules/language/src/Form/LanguageDeleteForm.php
+++ core/modules/language/src/Form/LanguageDeleteForm.php
@@ -3,7 +3,6 @@
 namespace Drupal\language\Form;
 
 use Drupal\Core\Entity\EntityDeleteForm;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Defines a confirmation form for deleting a language entity.
@@ -11,24 +10,6 @@ use Drupal\Core\Form\FormStateInterface;
  * @internal
  */
 class LanguageDeleteForm extends EntityDeleteForm {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $num_nodes = $this->entityTypeManager->getStorage('node')->getQuery()
-      ->condition('langcode', $this->entity->id())
-      ->count()
-      ->execute();
-    if ($num_nodes) {
-      $caption = '<p>' . $this->formatPlural($num_nodes, 'Language %type is used by contents on your site. You can not remove this language until you have removed all of the %type content.', '%type is used by @count pieces of content on your site. You may not remove %type until you have removed all of the %type content.', ['%type' => $this->entity->label()]) . '</p>';
-      $form['#title'] = $this->getQuestion();
-      $form['description'] = ['#markup' => $caption];
-      return $form;
-    }
-
-    return parent::buildForm($form, $form_state);
-  }
 
   /**
    * {@inheritdoc}
